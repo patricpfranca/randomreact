69331a9bb9c3967951fda13f5d8f4a4f85c19896
Patric França <patric.franca@hubfintech.com.br>
2019-02-06 12:49:30 -0200

    app de números randomicos

---
+++ index.android.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View, Button, AppRegistry } from 'react-native';
+
+const geraNumeroAleatorio = () => {
+  var numeroAleatorio = Math.random();
+  numeroAleatorio = Math.floor((numeroAleatorio * 10));
+  alert(numeroAleatorio);
+};
+
+const App = () => {
+  return (
+    <View>
+      <Text>Gerador de números randômicos!</Text>
+      <Button title="Gerar um número randômico" onPress={geraNumeroAleatorio} />
+    </View>
+  );
+};
+
+AppRegistry.registerComponent('app_1', () => App);
